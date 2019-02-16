ae64946bddcc68e530c0f547106b72386b69b6b1
elektros230 <krzysiek@biotronika.pl>
2019-02-16 17:36:39 +0100

    2019-02-16

--- miniVOLL.ino
+++ miniVOLL.ino
@@ -1,18 +1,29 @@
+/* miniVOLL is firmware software for Arduino miniVOLL device
+ * Author: Chris Czoba  krzysiek@biotronika.pl
+ *
+ * For more see: biotronics.eu
+ *
+ */
+
 #include <Arduino.h>      // For eclipse IDE only
 
-#define analogInPin  A3               // Analog input form transoptor
-#define oneBitEqivalentVoltage 3.2258 // 1023bits = 3.3V => 1bit = 3.2258mV
-#define maximumInputTresholdVoltage 3000 // e.g.: 3000mV => 3.3V - 0.30V
-#define minimumSignalTresholdVolatge 1000 //
+#define SOFT_VER "2019-02-16"
+#define HRDW_VER "NANO 1.0"
 
-long inputVoltage = 0;        // value read from the pot
+#define analogInPin	A3	// Analog input form transoptor
+#define buzerPin	D3	// For signal buzer
+#define oneBitEqivalentVoltage 3.2258 // 1023bits = 3.3V => 1bit = 3.2258mV
+#define maximumInputThresholdVoltage 3000 // Is used for calibration purpose. e.g.: 3000mV => 3.3V - 0.30V
+#define minimumSignalThresholdVolatge 1171 // Threshold of 0%
+
+long inputVoltage = 0;
 boolean started = false;
-long outputValue = 0;        // value output to the PWM (analog out)
+long outputValue = 0;
 
 void startGraph();
 
 void setup() {
-  analogReference(INTERNAL); //Change to 3.3V External arduino nano reference
+  analogReference(INTERNAL); //Change to 3.3V External Arduino NANO reference
   Serial.begin(115000);
   Serial.println(100);
   Serial.println(0);
@@ -22,17 +33,17 @@ void loop() {
 
   inputVoltage = analogRead(analogInPin)*oneBitEqivalentVoltage;
 
-  if (inputVoltage>minimumSignalTresholdVolatge && !started){
+  if (inputVoltage>minimumSignalThresholdVolatge && !started){
     started = true;
     startGraph();
   }
 
-  if (inputVoltage<=minimumSignalTresholdVolatge){
+  if (inputVoltage<=minimumSignalThresholdVolatge){
     started=false;
     delay(100);
   }
   if (started) {
-    outputValue = map(inputVoltage, 0, 3300, 0, 100);
+    outputValue = map(inputVoltage, 0, 3300, minimumSignalThresholdVolatge, 100);
     Serial.println(outputValue);
   }
 
